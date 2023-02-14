ab7748f6cd7f0b3c86cb23d7d14fbc092fed1bbc
TranNhatTan14 <97381710+TranNhatTan14@users.noreply.github.com>
2023-02-14 12:23:17 +0000

    Update

--- Scripts/Elevate.js
+++ Scripts/Elevate.js
@@ -2,7 +2,7 @@ let body = JSON.parse($response.body)
 
 body.user.is_on_free_trial = false;
 body.user.can_purchase = false;
-body.user.subscription_expiration_date_epoch = 7956915742;
+body.user.subscription_expiration_date_epoch = 4078684800;
 body.user.current_subscription.sku = "com.elevateapp.elevate.renewable.year_subscription_16";
 body.user.current_subscription.duration = "annual";
 body.user.current_subscription.store = "App Store";
