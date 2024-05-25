361006fe7cc05c736b1690f75bb67bbcaa5c0a11
Tran Nhat Tan <97381710+TranNhatTan14@users.noreply.github.com>
2024-05-25 06:03:18 +0000

    ELSA Speak

--- Scripts/elsa-reponse.js
+++ Scripts/elsa-reponse.js
@@ -2,10 +2,11 @@ let body = JSON.parse($response.body);
 
 if ($request.method == "GET") {
   body["subscriptions"] = [{
-    "expire_at": "21190505",
+    "expire_at": "22970710",
+    "store": "elsa-voucher",
     "subscription": "lifetime_membership",
-    "days_to_end": 36469,
-    "created_at": "20190529151809"
+    "days_to_end": 99758,
+    "created_at": "20230925113919"
   }];
   $done({body: JSON.stringify(body)});
 }
--- Scripts/elsa-request.js
+++ Scripts/elsa-request.js
@@ -1,4 +1,4 @@
 let headers = $request.headers
 
-headers["x-session-token"] = "F3S0w0bysBQFdbjtxpFurrFv2ItBBcBkVQxUddQW+9vjt2JXM751ksqq5GAWpkl+kk9nhig9BGh9JhYHQaokmendY6zLZDscHiRkZD2HrdJclKVCLordAARJhYIrf5C+5OSK6ax2TA45CKi8S09FEtYXN4noXO7gt42NT6WPIv6DKhdIwVxQuIAMLU5abmpMTDlyWeI4ulBWcOQbuZWWZg==";
+headers["x-session-token"] = "F3S0w0bysBQFdbjtxpFurowi5YrSJdA29rj0C1160nkvD1YjNQqJBbmcaZ+d/sY4wJUsU2zzKK5FfBl/6vkNNNSJdTDz24exKssxeCFaWO70/vMVX4FHGfIrdbGNp6ANQn2+3fEQDEDrBvGn9XgdVfPKUtHN94Y7xzW3KsDMWACpmPST+IV/vwCcpD1EbKaiCPlK7+FlwqNs24KiaJVV9g==";
 $done({headers})
