57fa4875478b0745bd718ef488f4425c16947c04
Tran Nhat Tan <97381710+TranNhatTan14@users.noreply.github.com>
2024-05-25 06:45:53 +0000

    ELSA request

--- Scripts/elsa-request.js
+++ Scripts/elsa-request.js
@@ -1,4 +1,4 @@
-let headers = $request.headers
+let header = $request.header
 
-headers["x-session-token"] = "F3S0w0bysBQFdbjtxpFurowi5YrSJdA29rj0C1160nkvD1YjNQqJBbmcaZ+d/sY4wJUsU2zzKK5FfBl/6vkNNNSJdTDz24exKssxeCFaWO70/vMVX4FHGfIrdbGNp6ANQn2+3fEQDEDrBvGn9XgdVfPKUtHN94Y7xzW3KsDMWACpmPST+IV/vwCcpD1EbKaiCPlK7+FlwqNs24KiaJVV9g==";
-$done({headers})
+header["x-session-token"] = "F3S0w0bysBQFdbjtxpFurowi5YrSJdA29rj0C1160nkvD1YjNQqJBbmcaZ+d/sY4wJUsU2zzKK5FfBl/6vkNNNSJdTDz24exKssxeCFaWO70/vMVX4FHGfIrdbGNp6ANQn2+3fEQDEDrBvGn9XgdVfPKUtHN94Y7xzW3KsDMWACpmPST+IV/vwCcpD1EbKaiCPlK7+FlwqNs24KiaJVV9g==";
+$done({header})
